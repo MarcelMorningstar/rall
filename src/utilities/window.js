9153c67706378a2f144b6cbcd05e8f77a6a3480b
Rolands Bluks <rolland1403@gmail.com>
2023-08-13 17:07:34 +0200

    useWindowDimesions error fix

--- src/utilities/window.js
+++ src/utilities/window.js
@@ -1,28 +1,25 @@
-import { useEffect, useState } from "react";
-
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-
-    return {
-        width,
-        height
-    };
-}
+import { useState, useLayoutEffect } from "react";
 
 export function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(
-        getWindowDimensions()
-    );
-  
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-  
-        window.addEventListener("resize", handleResize);
+    const [windowDimensions, setWindowDimensions] = useState({
+        width: 0,
+        height: 0,
+    });
+    
+    function handleResize() {
+        setWindowDimensions({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    };
+    
+    useLayoutEffect(() => {
+    handleResize();
 
-        return () => window.removeEventListener("resize", handleResize);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
     }, []);
-  
+    
     return windowDimensions;
 }
