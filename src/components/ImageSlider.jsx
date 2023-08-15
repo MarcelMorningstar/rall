812da473ce38a17cb5852d59ee9cf908d0acbb70
Rolands Bluks <rolland1403@gmail.com>
2023-08-15 21:10:37 +0200

    imageslider fix

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -8,60 +8,43 @@ import "slick-carousel/slick/slick-theme.css";
 export default function ImageSlider() {
     const settings = {
         dots: false,
+        arrows: false,
+        draggable: false,
         infinite: true,
-        speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         initialSlide: 0,
         autoplay: true,
-        speed: 12000,
+        speed: 6000,
         autoplaySpeed: 0,
         cssEase: "linear",
+        pauseOnDotsHover: false,
+        pauseOnFocus: false,
+        pauseOnHover: true,
         responsive: [
             {
                 breakpoint: 1550,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 2,
-                    initialSlide: 0,
-                    infinite: true,
-                    autoplay: true,
-                    speed: 10000,
-                    autoplaySpeed: 0,
-                    cssEase: "linear",
                 }
             },
             {
                 breakpoint: 1020,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1,
-                    initialSlide: 0,
-                    infinite: true,
-                    autoplay: true,
-                    speed: 8000,
-                    autoplaySpeed: 0,
-                    cssEase: "linear",
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 0,
-                    infinite: true,
-                    autoplay: true,
-                    speed: 7000,
-                    autoplaySpeed: 0,
-                    cssEase: "linear",
                 }
             },
         ]
     };
 
     return (
-        <Slider className="overflow-hidden" {...settings}>
+        <Slider {...settings}>
             <div className="relative h-80">
                 <Image src="/images/c2.jpg" className="object-cover" fill loading="eager" alt="rall contruction" />
             </div>
