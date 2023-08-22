95a8f12838b36a09bef409246acc1ba35d6d4a08
Rolands Bluks <rolland1403@gmail.com>
2023-08-23 01:03:38 +0200

    images

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -46,7 +46,7 @@ export default function ImageSlider({ images }) {
             {
                 images.map(image => (
                     <div key={image} className="relative h-80">
-                        <Image src={`/images/${image}`} className="px-1.5 object-cover" sizes="(max-width: 600px) 100vw, (max-width: 1020px) 50vw, 33vw" fill priority alt="rall contruction" />
+                        <img src={`/images/${image}`} className="w-full h-full px-1.5 object-cover" loading="eager" alt="rall contruction" />
                     </div>
                 ))
             }
