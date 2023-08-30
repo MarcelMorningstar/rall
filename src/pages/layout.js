07d85c88651435e9ad95daa9764d3151d73a0a3a
MarcelMorningstar <rolland1403@gmail.com>
2023-08-30 15:05:20 +0200

    meta description

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -2,15 +2,19 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import useTranslation from 'next-translate/useTranslation'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Layout({ children }) {
+  const { t } = useTranslation("home")
+
   return (
     <div className={inter.className}>
       <Head>
           <title>RALL</title>
           <link href="favicon.ico" type="image/x-icon" />
+          <meta name='discription' content={t("home:about-description")} />
       </Head>
       <Navbar />
       <main>{children}</main>
